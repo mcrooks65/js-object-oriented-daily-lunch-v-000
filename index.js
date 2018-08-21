e6d752995189f3fbf5d29bcb8c4e5e5c24db09d1
Matthew Crooks <mfcrooks@gmail.com>
2018-08-21 23:23:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,15 +8,20 @@ class Neighborhood {
     this.name = name;
     store.neighborhoods.push(this);
   }
-  delivers() {
-    return // returns a list of all deliveries placed in a neighborhood
-  }
-  customers() {
+  customers() { // returns all of the customers that live in a particular neighborhood
+     return store.customers.filter(customer => customer.neighborhoodId === this.id);
+   }
 
-  }
-  meals() {
+   meals() { // returns a unique list of meals that have been ordered in a particular neighborhood (you might want to do this one last)
+Customer class:
+     const allMeals = this.customers().map(customer => customer.meals());
+     const merged = [].concat.apply([], allMeals);
+     return [...new Set(merged)];
+   }
 
-  }
+   deliveries() { // returns a list of all deliveries placed in a neighborhood
+     return store.deliveries.filter(delivery => delivery.neighborhoodId === this.id);
+   }
 }
 
 class Customer {
