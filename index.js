a73b71b742d30acd10ff045396e523ad21390c46
Matthew Crooks <mfcrooks@gmail.com>
2018-08-21 23:25:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
-class Neighborhood {
-  let neighborhoodId = 1;
-  constructor(name) {
-    this.id = neighborhoodId++;
-    this.name = name;
-    store.neighborhoods.push(this);
-  }
-  customers() { // returns all of the customers that live in a particular neighborhood
-     return store.customers.filter(customer => customer.neighborhoodId === this.id);
-   }
+const Neighborhood = (() => {
+  let neighborhoodIds = 1;
+  return class {
+    constructor(name) {
+      this.id = neighborhoodIds++;
+      this.name = name;
+      store.neighborhoods.push(this);
+    }
 
-   meals() { // returns a unique list of meals that have been ordered in a particular neighborhood (you might want to do this one last)
-Customer class:
-     const allMeals = this.customers().map(customer => customer.meals());
-     const merged = [].concat.apply([], allMeals);
-     return [...new Set(merged)];
-   }
+    customers() {
+      return store.customers.filter(customer => customer.neighborhoodId === this.id);
+    }
 
-   deliveries() { // returns a list of all deliveries placed in a neighborhood
-     return store.deliveries.filter(delivery => delivery.neighborhoodId === this.id);
-   }
-}
+    meals() {
+      const allMeals = this.customers().map(customer => customer.meals());
+      const merged = [].concat.apply([], allMeals);
+      return [...new Set(merged)];
+    }
 
-class Customer {
-  constructor(name, neighborhoodId) {
-    this.name = name;
-    this.id = neighborhoodId++;
-  }
-}
+    deliveries() {
+      return store.deliveries.filter(delivery => delivery.neighborhoodId === this.id);
+    }
+  };
+})();
