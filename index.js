7cebe632392470a2883ecb79359f2f5a4b886cbe
Matthew Crooks <mfcrooks@gmail.com>
2018-08-21 23:14:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
-let costumerId = 0
+let costumerId = 0;
 
 class Customer {
   constructor(name) {
     this.name = name;
     this.id = customerId++;
+  }
+  delivers() {
+    return // returns a list of all deliveries placed in a neighborhood
   }
 }
