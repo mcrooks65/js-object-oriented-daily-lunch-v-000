675a02eb3c2261935966ece85d7d6f335016385d
Matthew Crooks <mfcrooks@gmail.com>
2018-08-21 23:13:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+class Customer {
+  constructor(name) {
+    this.name = name;
+    this.id = customerId++;
+  }
+}
